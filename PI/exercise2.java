95d21d91bdcff774510ea966b56f268d2d3c46a6
Caio Moraes <caioms001@gmail.com>
2018-03-02 00:13:04 -0300

    Update

--- PI/exercise2.java
+++ PI/exercise2.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class atividade2{
+public class exercise2{
 	public static void main(String[] args) {
 		Scanner entry = new Scanner(System.in);
 		int number1 = entry.nextInt();
 		int number2 = entry.nextInt();
-		int div1 = number2%2;
+		int div1 = number1%2;
 		int div2 = number2%2;
-		if (div1 == 0 && div2 == 0) {
+		if ((div1 == 0) && (div2 == 0)) {
 			System.out.println("The two numbers entered are even.");
 		}
-		if else (div1 == 1 && div2 == 1) {
+		else if ((div1 == 1) && (div2 == 1)) {
 			System.out.println("The two numbers entered are odd.");
 		}
 		else {
---
+++ ThaisClass/ThaisClass.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ThaisClass{
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int preço;
+		System.out.print("Media final = ");
+	}
+}
