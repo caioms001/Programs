7b62a138994ca0eab350caab72feb588b0975aa0
Caio Moraes <caioms001@gmail.com>
2018-03-14 22:23:55 -0300

    Update

---
+++ 2. PI/03.14/exercise3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class exercise20{
+	public static void main(String[] args) {
+		Scanner entry = new Scanner(System.in);
+		int aDay, aMonth, aYear, bDay, bMonth, bYear;
+		do {
+			System.out.print("Enter the atual day in this format 14 03 2018: ");
+			aDay = entry.nextInt();
+			aMonth = entry.nextInt();
+			aYear = entry.nextInt();
+			System.out.print("Enter the birthday in this format 30 10 1998: ");
+			bDay = entry.nextInt();
+			bMonth = entry.nextInt();
+			bYear = entry.nextInt();
+		} while ()
+	}
+}
