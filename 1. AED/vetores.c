5bf2d7398c64a739b47b80260318ae3c762323a9
Caio Moraes <caioms001@gmail.com>
2018-05-30 06:40:21 -0300

    Update

--- 1. AED/vetores.c
+++ 1. AED/vetores.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main (void) {
-	int idade[10] = {1,2,3,4,5,6,7,8,9,10};
-	printf("*vetor: %d\n", *(idade+1));
-	// printf("*vetor + 1: %d\n",*(vetor+1));
+	int idade;
+	// int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
 	// printf("vetor: %p\n", vetor);
-	printf("&vetor[0]: %p\n", &idade[0]);
-	// printf("%d\n", vetor[6]);
+	// printf("*vetor: %d\n", *(vetor));
+	// printf("*vetor + 1: %d\n",*(vetor+1));
+	// printf("&vetor[0]: %p\n", &vetor[0]);
+	// printf("&vetor[1]: %p\n", &vetor[1]);
+	printf("%p\n", &idade =);
 }
--- Head First/sizeof.c
+++ Head First/sizeof.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 
 int main(){
-	char varInt = 'C';
-	 varInt2 = 'C';
-	for (int i = 0; i < 300; ++i)
-	{
-		/* code */
-	}
-
-	//char testeS[] = "He";
-	printf("Tamanho: %p\n", &varInt);
-	printf("Tamanho: %p\n", &varInt2);
+	printf("Tamanho: %lu\n", sizeof(long long double));
 	return 0;
 }
